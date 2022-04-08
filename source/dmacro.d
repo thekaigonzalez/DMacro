98f144024bcff0e782dca5db36e9cea398a49c7d
Kai Gonzalez <gkai70264@gmail.com>
2022-04-08 15:57:37 -0400

    Update - Add errors

--- source/dmacro.d
+++ source/dmacro.d
@@ -9,6 +9,14 @@ import std.array;
 
 import applogger;
 
+class DMUnfinishedException : Exception {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable nextInChain = null) pure nothrow @nogc @safe
+    {
+        super(msg, file, line, nextInChain);
+    }
+}
+
+
 /// generate a dictionary from parsed DMacro code
 string[][string] parseDM(string s) {
     
@@ -51,6 +59,13 @@ string[][string] parseDM(string s) {
         else {
             buffer ~= sw;
         }
+    }
+    if (state == 999) {
+        errorMessage("Error: unfinished string.");
+        throw new DMUnfinishedException("Unfinished string");
+    } else if (state == 1) {
+        errorMessage("Error: Unfinished macro name at end of file.");
+        throw new DMUnfinishedException("Unfinished `macro_name'");
     }
     return fB;
 }
